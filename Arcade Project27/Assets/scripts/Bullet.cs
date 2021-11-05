f1800c348a5df21abfd4de8047b02bb974509012
JohnyWuijtsNL <J.wuijts@sintlucasedu.nl>
2021-11-05 12:47:26 +0100

    Added bullet velocity and rotation

--- Arcade Project27/Assets/scripts/Bullet.cs
+++ Arcade Project27/Assets/scripts/Bullet.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    Vector2 oldPos;
+    float lookDirection;
+    private void Update()
+    {
+        if (oldPos == null)
+        {
+            oldPos = transform.position;
+        }
+        lookDirection = -Vector2.Angle(Vector2.up, oldPos);
+        transform.eulerAngles = new Vector3(0, 0, lookDirection);
 
+        oldPos = transform.position;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- Arcade Project27/Assets/scripts/PlayerScript.cs
+++ Arcade Project27/Assets/scripts/PlayerScript.cs
@@ -14,6 +14,8 @@ public class PlayerScript : MonoBehaviour
     [SerializeField]
     GameObject shootPosition;
     Vector3 shootDirection;
+    Vector3 oldPos;
+    Vector3 playerVel;
 
 
     Vector2 movement;
@@ -21,18 +23,21 @@ public class PlayerScript : MonoBehaviour
 
     public GameObject bulletPrefab;
 
-    public float bulletForce = 20f;
+    public float bulletForce = 200f;
 
     void Update()
     {
+        if (oldPos == null)
+        {
+            oldPos = transform.position;
+        }
+        Debug.Log(playerVel);
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         shootDirection = Vector3.ClampMagnitude(new Vector3(Input.GetAxisRaw("HorizontalShoot"), -Input.GetAxisRaw("VerticalShoot"), 0), 1) * 0.2f;
         shootPosition.transform.position = transform.position + shootDirection;
         bulletTimer -= Time.deltaTime;
 
-
-        Debug.Log(Vector3.Angle(transform.position, shootPosition.transform.position));
 
         if (new Vector2(Input.GetAxisRaw("HorizontalShoot"), -Input.GetAxisRaw("VerticalShoot")) != new Vector2(0, 0))
         {
@@ -41,13 +46,11 @@ public class PlayerScript : MonoBehaviour
 
         void Shoot()
         {
-            shootPosition.transform.LookAt(-transform.position);
-            shootPosition.transform.eulerAngles = new Vector3(0, 0, shootPosition.transform.eulerAngles.z);
             if (bulletTimer <= 0)
             {
-                GameObject bullet = Instantiate(bulletPrefab, shootPosition.transform.position, shootPosition.transform.rotation);
+                GameObject bullet = Instantiate(bulletPrefab, shootPosition.transform.position, Quaternion.identity);
                 Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-                rb.AddForce(shootDirection * bulletForce, ForceMode2D.Impulse);
+                rb.AddForce((shootDirection * bulletForce) + playerVel * 50, ForceMode2D.Impulse);
                 bulletTimer = bulletDelay;
             }
 
@@ -58,5 +61,7 @@ public class PlayerScript : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        playerVel = (transform.position - oldPos);
+        oldPos = transform.position;
     }
 }
